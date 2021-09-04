4b84f6ee9a021e14f15253816575f8796ed0b1f6
Berrybunny015 <berrybunny015@gmail.com>
2021-09-04 19:04:37 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main( String[] args ) {
+
+        System.out.println("\nWhat is the quote? "); //output #1
+
+        Scanner scannedQuote = new Scanner(System.in); //scan for quote
+        String quote= scannedQuote.nextLine();
+
+        System.out.println("\nWho said it? "); //output #2
+
+        Scanner scannedAuthor = new Scanner(System.in); //scan for author who said quote
+        String author= scannedAuthor.nextLine();
+
+        System.out.println("\n" + author + " says, " + "\"" + quote + "\""); //output #3
+
+    }
+}
